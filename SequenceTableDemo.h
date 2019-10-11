f509512555c98ac2dd728f840975dbbee5d7cef4
墨茗 <55228896+INK-RAIN@users.noreply.github.com>
2019-10-10 21:27:23 -0500

    解决了一大堆bug，不过仍有问题
    在测试中，插入函数仍存在问题，极有可能是无法插入

--- SequenceTableDemo.h
+++ SequenceTableDemo.h
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <math.h>
+#include <ctime>
+#include <cmath>
+typedef int status;
 
 constexpr auto Increased_Memory_Space = 10;
 constexpr auto SafeSpace = 3;
@@ -16,19 +19,116 @@ typedef struct {
 	int LastSize;
 }Sqtable;
 
-
+int MSqt_FillWithZero(Sqtable& L);
+//用null填满
+int MSqt_FillWithNULL(Sqtable& L) {
+	for (int i = 0; i <= L.Length - 1; i++) {
+		L.elements[i] = NULL;
+	}
+	return True;
+}
 //初始化顺序表
-int MSqt_Initial(Sqtable &L) {
+int MSqt_Initial(Sqtable& L) {
 	//基地址
 	L.elements = (int*)malloc(InitialSpace * sizeof(int));
 	//如果创建失败，返回false
-	if (L.elements==NULL) {
+	if (L.elements == NULL) {
 		return 1001;
 	}
 	//长度
 	L.Length = 0;
 	//实际空间
 	L.LastSize = InitialSpace;
+	return True;
+}
+
+//顺序表交换元素位置
+int MSqt_SwapElems(Sqtable& L, int location_a, int location_b) {
+	int a = L.elements[location_a];
+	L.elements[location_a] = L.elements[location_b];
+	L.elements[location_b] = a;
+	return True;
+}
+
+
+//顺序表重置
+int MSqt_Clear(Sqtable& L) {
+	MSqt_Initial(L);
+	MSqt_FillWithNULL(L);
+	return True;
+}
+
+
+//判断是否为空表
+int MSqt_IsEmpty(Sqtable L) {
+	if (L.Length == 0) {
+		return True;
+	}
+	else
+	{
+		return False;
+	}
+	return True;
+}
+
+
+//用0填满
+int MSqt_FillWithZero(Sqtable& L) {
+	for (int i = 0; i <= L.Length - 1; i++) {
+		L.elements[i] = 0;
+	}
+	return True;
+}
+
+
+//手动扩容
+int MSqt_ExtendedSpace(Sqtable& L, int Size) {
+	//合法性检查
+	if (Size <= 0) {
+		return  2001;
+	}
+	int* NewPoint = (int*)realloc(L.elements, (L.LastSize + Size) * sizeof(int));
+	//检查realloc函数是否分配失败，使用中间量避免影响顺序表
+	if (NewPoint != NULL) {
+		//指针更新
+		L.elements = NewPoint;
+		//容量更新
+		L.LastSize = L.LastSize + Size;
+	}
+	else {
+		//如果分配失败，拒绝赋值给L.elements,避免野指针出现
+		return 1002;
+	}
+	return True;
+}
+
+
+
+
+//顺序表末尾添加操作
+int MSqt_Add(Sqtable& L, int elem) {
+	//检查安全空间是否足够，不足的话增加空间
+	if (L.LastSize - L.Length <= SafeSpace) {
+		int* NewPoint = (int*)realloc(L.elements, (L.LastSize + Increased_Memory_Space) * sizeof(int));
+		//检查realloc函数是否分配失败，使用中间量避免影响顺序表
+		if (NewPoint != NULL) {
+			//指针更新
+			L.elements = NewPoint;
+			//容量更新
+			L.LastSize = L.LastSize + Increased_Memory_Space;
+		}
+		else {
+			//如果分配失败，拒绝赋值给L.elements,避免野指针出现
+			return 1004;
+		}
+	}
+	//检查合法性(Do Something)
+
+
+	//添加操作
+	L.elements[L.Length - 1] = elem;
+	//增加长度计数
+	L.Length++;
 	return True;
 }
 
@@ -67,32 +167,7 @@ int MSqt_Insert(Sqtable &L,int elem,int location) {
 }
 
 
-//顺序表末尾添加操作
-int MSqt_Add(Sqtable &L,int elem) {
-	//检查安全空间是否足够，不足的话增加空间
-	if (L.LastSize - L.Length <= SafeSpace) {
-		int* NewPoint = (int*)realloc(L.elements, (L.LastSize + Increased_Memory_Space) * sizeof(int));
-		//检查realloc函数是否分配失败，使用中间量避免影响顺序表
-		if (NewPoint != NULL) {
-			//指针更新
-			L.elements = NewPoint;
-			//容量更新
-			L.LastSize = L.LastSize + Increased_Memory_Space;
-		}
-		else {
-			//如果分配失败，拒绝赋值给L.elements,避免野指针出现
-			return 1004;
-		}
-	}
-	//检查合法性(Do Something)
 
-
-	//添加操作
-	L.elements[L.Length - 1] = elem;
-	//增加长度计数
-	L.Length++;
-	return True;
-}
 
 
 //顺序表销毁操作
@@ -133,24 +208,121 @@ int MSqt_Drop(Sqtable &L,int location) {
 	return True;
 }
 
-//尝试使用计数排序
-//顺序表排序（总）
-int MSqt_Sort(Sqtable &L,int SortType) {
-	//判断合法性，排序需要至少两个元素
-	//参数0是正向排序，1是反向排序
-	if (L.Length < 2) {
-		return 1008;
+
+int FinalResultOfNumCoreP(Sqtable& PositiveNumberCount, int SortType, Sqtable& L) {
+	if (SortType == 0) {
+		//操作表地址值
+		int location = 0;
+		//直接遍历正表并输出
+		for (int i = 0; i <= PositiveNumberCount.Length - 1; i++) {
+			while (PositiveNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = i;
+				PositiveNumberCount.elements[i]--;
+				location++;
+			}
+		}
 	}
-	if ((SortType = 0 )|| (SortType = 1)) {
-		return NumCore(L, SortType);
+	//结束
+	//如果倒序，就倒着遍历正表
+	else {
+		//操作表地址值
+		int location = 0;
+		for (int i = PositiveNumberCount.Length - 1; i >= 0; i--) {
+			while (PositiveNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = i;
+				PositiveNumberCount.elements[i]--;
+				location++;
+			}
+		}
+	}
+	return True;
+
+}
+int FinalResultOfNumCore(Sqtable& PositiveNumberCount, Sqtable& NegativeNumberCount, int SortType, Sqtable& L) {
+	//正向排序
+	if (SortType == 0) {
+		//操作表地址值
+		int location = 0;
+		//首先倒着遍历负表，用负值输出
+		for (int i = NegativeNumberCount.Length - 1; i >= 0; i--) {
+			while (NegativeNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = -i;
+				NegativeNumberCount.elements[i]--;
+				location++;
+			}
+		}
+		//然后正着遍历正表，接着负表末尾输出
+		for (int i = 0; i <= PositiveNumberCount.Length - 1; i++) {
+			while (PositiveNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = i;
+				PositiveNumberCount.elements[i]--;
+				location++;
+			}
+		}
+	}
+	//正向排序操作结束
+	//如果参数为负向排序
+	else {
+		//操作表地址值
+		int location = 0;
+		//首先倒着遍历正表
+		for (int i = PositiveNumberCount.Length - 1; i >= 0; i--) {
+			while (PositiveNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = i;
+				PositiveNumberCount.elements[i]--;
+				location++;
+			}
+		}
+		//然后遍历负表，用负值输出
+		for (int i = 0; i <= NegativeNumberCount.Length - 1; i++) {
+			while (NegativeNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = -i;
+				NegativeNumberCount.elements[i]--;
+				location++;
+			}
+		}
+	}
+	//负向排序结束
+	return True;
+}
+int FinalResultOfNumCoreN(Sqtable& NegativeNumberCount, int SortType, Sqtable& L) {
+	if (SortType == 0) {
+		//正向排序，则倒着遍历负表，用负值输出
+		//操作表地址值
+		int location = 0;
+		for (int i = NegativeNumberCount.Length - 1; i >= 0; i--) {
+			while (NegativeNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = -i;
+				NegativeNumberCount.elements[i]--;
+				location++;
+			}
+		}
 	}
 	else {
-		return  1007;
+		//负向排序，则直接遍历负表，用负值输出
+		//操作表地址值
+		int location = 0;
+		for (int i = 0; i <= NegativeNumberCount.Length - 1; i++) {
+			while (NegativeNumberCount.elements[i] != 0)
+			{
+				L.elements[location] = -i;
+				NegativeNumberCount.elements[i]--;
+				location++;
+			}
+		}
 	}
+	return True;
 }
 
 //计数排序核心
-int NumCore(Sqtable& L,int SortType) {
+int NumCore(Sqtable& L, int SortType) {
 	//获取计数排序范围
 	int MinNum, MaxNum;
 	for (int i = 0; i <= L.Length - 1; i++) {
@@ -230,190 +402,42 @@ int NumCore(Sqtable& L,int SortType) {
 	}
 	return True;
 }
-//计数核心之结果处理器
-int FinalResultOfNumCoreP(Sqtable& PositiveNumberCount,int SortType,Sqtable &L) {
-	if (SortType = 0) {
-		//操作表地址值
-		int location = 0;
-		//直接遍历正表并输出
-		for (int i = 0; i <= PositiveNumberCount.Length - 1; i++) {
-			while (PositiveNumberCount.elements[i] != 0)
-			{
-				L.elements[location] = i;
-				PositiveNumberCount.elements[i]--;
-				location++;
-			}
-		}
+//尝试使用计数排序
+//顺序表排序（总）
+int MSqt_Sort(Sqtable &L,int SortType) {
+	//判断合法性，排序需要至少两个元素
+	//参数0是正向排序，1是反向排序
+	if (L.Length < 2) {
+		return 1008;
 	}
-	//结束
-	//如果倒序，就倒着遍历正表
-	else {
-		//操作表地址值
-		int location = 0;
-		for (int i = PositiveNumberCount.Length - 1; i >= 0; i--) {
-			while (PositiveNumberCount.elements[i] != 0)
-			{
-				L.elements[location] = i;
-				PositiveNumberCount.elements[i]--;
-				location++;
-			}
-		}
-	}
-	return True;
-	
-}
-int FinalResultOfNumCore(Sqtable& PositiveNumberCount, Sqtable& NegativeNumberCount, int SortType, Sqtable &L) {
-	//正向排序
-	if (SortType = 0) {
-		//操作表地址值
-		int location = 0;
-		//首先倒着遍历负表，用负值输出
-		for (int i = NegativeNumberCount.Length-1; i >= 0; i--) {
-			while (NegativeNumberCount.elements[i]!= 0)
-			{
-				L.elements[location] = -i;
-				NegativeNumberCount.elements[i]--;
-				location++;
-			}
-		}
-		//然后正着遍历正表，接着负表末尾输出
-		for (int i = 0; i <= PositiveNumberCount.Length - 1; i++) {
-			while (PositiveNumberCount.elements[i]!=0)
-			{
-				L.elements[location] = i;
-				PositiveNumberCount.elements[i]--;
-				location++;
-			}
-		}
-	}
-	//正向排序操作结束
-	//如果参数为负向排序
-	else {
-		//操作表地址值
-		int location = 0;
-		//首先倒着遍历正表
-		for (int i = PositiveNumberCount.Length - 1; i >= 0; i--) {
-			while (PositiveNumberCount.elements[i] != 0)
-			{
-				L.elements[location] = i;
-				PositiveNumberCount.elements[i]--;
-				location++;
-			}
-		}
-		//然后遍历负表，用负值输出
-		for (int i = 0; i <= NegativeNumberCount.Length - 1; i++) {
-			while (NegativeNumberCount.elements[i] != 0)
-			{
-				L.elements[location] = -i;
-				NegativeNumberCount.elements[i]--;
-				location++;
-			}
-		}
-	}
-	//负向排序结束
-	return True;
-}
-int FinalResultOfNumCoreN(Sqtable& NegativeNumberCount, int SortType, Sqtable &L) {
-	if (SortType = 0) {
-		//正向排序，则倒着遍历负表，用负值输出
-		//操作表地址值
-		int location = 0;
-		for (int i = NegativeNumberCount.Length - 1; i >= 0; i--) {
-			while (NegativeNumberCount.elements[i] != 0)
-			{
-				L.elements[location] = -i;
-				NegativeNumberCount.elements[i]--;
-				location++;
-			}
-		}
+	if ((SortType == 0 )|| (SortType == 1)) {
+		return NumCore(L, SortType);
 	}
 	else {
-		//负向排序，则直接遍历负表，用负值输出
-		//操作表地址值
-		int location = 0;
-		for (int i = 0; i <= NegativeNumberCount.Length - 1; i++) {
-			while (NegativeNumberCount.elements[i] != 0)
-			{
-				L.elements[location] = -i;
-				NegativeNumberCount.elements[i]--;
-				location++;
-			}
-		}
+		return  1007;
 	}
-	return True;
 }
 
 
-//顺序表交换元素位置
-int MSqt_SwapElems(Sqtable &L, int location_a,int location_b) {
-  int a =	L.elements[location_a];
-  L.elements[location_a] = L.elements[location_b];
-  L.elements[location_b] = a;
-  return True;
-}
 
+//两顺序表归并
+int MSqt_Merge(Sqtable &MainTable, Sqtable OtherTable,bool IsOrderly,bool CanNumberRepeat,bool RepeatOperationOnMainTable) {
+	if (RepeatOperationOnMainTable) {
 
-//顺序表重置
-int MSqt_Clear(Sqtable &L) {
-	MSqt_Initial(L);
-	MSqt_FillWithNULL(L);
-	return True;
-}
-
-
-//判断是否为空表
-int MSqt_IsEmpty(Sqtable L) {
-	if (L.Length = 0) {
-		return True;
-	}
-	else
-	{
-		return False;
-	}
-	return True;
-}
-
-
-//用0填满
-int MSqt_FillWithZero(Sqtable &L) {
-	for (int i = 0; i <= L.Length - 1; i++) {
-		L.elements[i] = 0;
-	}
-	return True;
-}
-//用null填满
-int MSqt_FillWithNULL(Sqtable& L) {
-	for (int i = 0; i <= L.Length - 1; i++) {
-		L.elements[i] = NULL;
-	}
-	return True;
-}
-
-//手动扩容
-int MSqt_ExtendedSpace(Sqtable& L,int Size) {
-	//合法性检查
-	if (Size <= 0) {
-		return  2001;
-	}
-	int* NewPoint = (int*)realloc(L.elements, (L.LastSize + Size) * sizeof(int));
-	//检查realloc函数是否分配失败，使用中间量避免影响顺序表
-	if (NewPoint != NULL) {
-		//指针更新
-		L.elements = NewPoint;
-		//容量更新
-		L.LastSize = L.LastSize + Size;
 	}
 	else {
-		//如果分配失败，拒绝赋值给L.elements,避免野指针出现
-		return 1002;
+
 	}
-	return True;
+	return 0;
 }
+
+
+
 
 
 ////////////////////////////////////关于顺序表的实现暂时告一段落，下面是关于链表的实现//////////////////////////
-typedef struct {
-	LinkTable* LTPoint = NULL;
+typedef struct LinkTable{
+	struct LinkTable* LTPoint = NULL;
 	int *element = (int*)malloc(1 * sizeof(int));
 }LinkTable,*linkTable;
 
@@ -446,13 +470,14 @@ int LT_Initial(LinkTable &L) {
 
 //链表新增数据
 int LT_AddElements(LinkTable &L,int elemnet) {
-	LinkTable *TemporaryTable;
+	LinkTable* TemporaryTable = NULL;
+	
 	TemporaryTable->LTPoint = L.LTPoint;
-	while (*TemporaryTable->LTPoint!=NULL)
+	while (TemporaryTable->LTPoint==NULL)
 	{
 		TemporaryTable = TemporaryTable->LTPoint;
 	}
-	LinkTable *NewLTable;
+	LinkTable* NewLTable = NULL;
 	TemporaryTable->LTPoint = NewLTable;
 	*NewLTable->element = elemnet;
 	return True;
@@ -462,9 +487,9 @@ int LT_AddElements(LinkTable &L,int elemnet) {
 //查询链表长度
 int LT_SelectLength(LinkTable L) {
 	int Long = 1;
-	LinkTable* TemporaryTable;
+	LinkTable* TemporaryTable = NULL;
 	TemporaryTable->LTPoint = L.LTPoint;
-	while (*TemporaryTable->LTPoint != NULL)
+	while (TemporaryTable->LTPoint!=NULL)
 	{
 		TemporaryTable = TemporaryTable->LTPoint;
 		Long++;
@@ -476,5 +501,5 @@ int LT_SelectLength(LinkTable L) {
 
 //链表插入数据
 int LT_InsertElements(LinkTable& L, int location, int element) {
-
+	return 0;
 }
